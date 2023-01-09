6944e0adfac1c3f3f743ae7ff03e589958c87c63
Richcy Dian Sukma <richcyveliz@gmail.com>
2023-01-09 11:54:56 +0700

    Checkpoint study: 12:00

---
+++ DotnetEfCore/dotnet-ef-core/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_ef_core.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=ef_db;uid=root;pwd=1234;");
+        }
+
+        public DbSet<CategoryEntity> categories { get; set; }
+    }
+}
